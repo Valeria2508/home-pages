165d27681ac7c0192a7c91e9626b43a0318339a6
val <valeria@gmail.com>
2024-12-02 08:54:21 -0500

    fix: vite config

--- vit.config.js
+++ vit.config.js
@@ -1,5 +1,5 @@
 export default {
-    base: '/home-pages/', // Reemplaza <REPO-NAME> con el nombre de tu repositorio
+    base: '/home2/', // Reemplaza <REPO-NAME> con el nombre de tu repositorio
     build: {
         outDir: 'dist', // Carpeta de salida para el build
     },
